4c1571323e6fc0ab8a81806636d8f7b3e5b0ed45
Gilbert <zjx1997@163.com>
2018-04-26 20:33:46 +0800

    gg

--- js/index.js
+++ js/index.js
@@ -7,9 +7,9 @@
     				anchors: ['遇见', '聊天', '电影','礼物', '济南', '槟城','约饭','我房间','面包','表白'],
     				sectionsColor: ['#f2f2f2', '#1BBC9B', '#7E8F7C', '#C63D0F', '#C63D0F', '#C63D0F', '#C63D0F', '#C63D0F', '#C63D0F', '#C63D0F'],
             verticalCentered: false,
-    				navigation: true,
-    				navigationPosition: 'right',
-    				navigationTooltips: ['遇见', '聊天', '电影','礼物', '济南', '槟城','约饭','我房间','面包','表白'],
+    				// navigation: true,
+    				// navigationPosition: 'right',
+    				// navigationTooltips: ['遇见', '聊天', '电影','礼物', '济南', '槟城','约饭','我房间','面包','表白'],
     				responsiveWidth: 900,
             onLeave: function(index, nextIndex, direction){
                   if(direction == "up"){
