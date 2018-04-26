51666b6ef25bde58ab92c9b9f7f4948c8de40e9c
Gilbert <zjx1997@163.com>
2018-04-26 22:16:09 +0800

    gg

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
   $(document).ready(function() {
           $('#homepage').fullpage({
-            scrollingSpeed: 1000,
+            scrollingSpeed: 500,
             autoScrolling: true,
-            fitToSection: true,
-            fitToSectionDelay: 2000,
+            // fitToSection: true,
+            // fitToSectionDelay: 2000,
     				anchors: ['home', 'about-us', 'what-we-do', 'our-work', 'contact1'],
-    				sectionsColor: ['#f2f2f2', '#1BBC9B', '#7E8F7C', '#C63D0F','#7E8F7C'],
+    				sectionsColor: ['#f2f2f2', '#1BBC9B', '#7E8F7C', '#C63D0F','#C63D0F'],
             verticalCentered: false,
     				// navigation: true,
     				// navigationPosition: 'right',
